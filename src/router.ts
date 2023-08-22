6776a525a615665a1472842a707f1b6a996062c7
= <=>
2023-08-22 19:34:05 +1200

    .

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Router } from 'express';
 import {glob} from 'glob'
 import path from 'path';
 import logger from './util/logger';
@@ -25,4 +25,4 @@ for (const [path,method,auth,handler] of routesList) {
 
 logger.info("exporting router");
 
-export default router
+export default router as Router;
